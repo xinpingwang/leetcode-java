1b1f05587feb5a41d8f76f13053f56fae3b5091e
XinPing Wang <xinping.wang@hotmail.com>
2019-12-25 12:58:18 +0800

    New: implement strstr

---
+++ src/main/java/com/xinping/leetcode/ImplementStrStr.java
@@ -0,0 +1,33 @@
+package com.xinping.leetcode;
+
+public class ImplementStrStr {
+
+    /**
+     * 暴力法
+     * <p>
+     * 3ms, 41.30%
+     */
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+
+        boolean find;
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            find = true;
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    find = false;
+                    break;
+                }
+            }
+            if (find) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    // TODO: KMP 算法
+}
