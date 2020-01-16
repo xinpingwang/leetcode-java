ea86d9126ef39006aecd72674dc123b235502306
XinPing Wang <xinping.wang@hotmail.com>
2020-01-16 10:24:14 +0800

    Add solution for minimum path sum

--- src/main/java/com/xinping/leetcode/solutions/MinimumPathSum.java
+++ src/main/java/com/xinping/leetcode/solutions/MinimumPathSum.java
@@ -2,6 +2,11 @@ package com.xinping.leetcode.solutions;
 
 public class MinimumPathSum {
 
+    /**
+     * dp, 二维数组
+     * <p>
+     * 3ms
+     */
     public int minPathSum(int[][] grid) {
         if (grid.length == 0 || grid[0].length == 0) {
             return 0;
@@ -28,5 +33,35 @@ public class MinimumPathSum {
         }
 
         return sum[h - 1][w - 1];
+    }
+
+    /**
+     * dp，一维数组
+     * <p>
+     * 2ms, 98.94%
+     */
+    public int minPathSum1(int[][] grid) {
+
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int h = grid.length, w = grid[0].length;
+
+        int[] dp = new int[w];
+        dp[0] = grid[0][0];
+
+        for (int j = 1; j < w; j++) {
+            dp[j] = dp[j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < h; i++) {
+            dp[0] = dp[0] + grid[i][0];
+            for (int j = 1; j < w; j++) {
+                dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[w - 1];
     }
 }
