02f7f295df7944e30fea74618bb31b5fd015608d
XinPing Wang <xinping.wang@hotmail.com>
2020-01-20 21:09:45 +0800

    New: nim game

---
+++ src/main/java/com/xinping/leetcode/solutions/NimGame.java
@@ -0,0 +1,13 @@
+package com.xinping.leetcode.solutions;
+
+public class NimGame {
+
+    /**
+     * 分析
+     * <p>
+     * 0ms, 100%
+     */
+    public boolean canWinNim(int n) {
+        return (n % 4 != 0);
+    }
+}
