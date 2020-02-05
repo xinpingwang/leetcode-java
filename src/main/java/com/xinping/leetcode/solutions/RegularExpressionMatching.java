cacebeff9db98fa94ab76c687e2cf89dc3e3708d
XinPing Wang <xinping.wang@hotmail.com>
2020-02-05 10:45:28 +0800

    New: regular expression matching

---
+++ src/main/java/com/xinping/leetcode/solutions/RegularExpressionMatching.java
@@ -0,0 +1,44 @@
+package com.xinping.leetcode.solutions;
+
+public class RegularExpressionMatching {
+
+    /**
+     * 回溯
+     * <p>
+     * 116ms, 9.87%
+     */
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) {
+            return s.isEmpty();
+        }
+        boolean firstMatch = !s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.');
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p));
+        } else {
+            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+
+    /**
+     * 动态规划
+     * <p>
+     * 3ms, 89.82%
+     */
+    public boolean isMatch1(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[s.length()][p.length()] = true;
+
+        for (int i = s.length(); i >= 0; i--) {
+            for (int j = p.length() - 1; j >= 0; j--) {
+                boolean firstMatch = i < s.length() && (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.');
+                if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+                    dp[i][j] = dp[i][j + 2] || firstMatch && dp[i + 1][j];
+                } else {
+                    dp[i][j] = firstMatch && dp[i + 1][j + 1];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
